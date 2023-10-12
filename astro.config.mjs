620df777c06d7844acfb9862e9248e3a83e89e17
Chike Ozulumba <o.devcode@gmail.com>
2023-10-12 11:52:23 +0100

    Cloudflare pages - update project config(2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true
-  })
+//   adapter: netlify({
+//     edgeMiddleware: true
+//   }),
+  adapter: cloudflare()
 });
