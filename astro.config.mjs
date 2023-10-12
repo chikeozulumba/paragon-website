f7cbc1da21b00c618cec8b659fd7d3cb8f0cf01e
Chike Ozulumba <o.devcode@gmail.com>
2023-10-12 12:44:58 +0100

    update config - 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-import netlify from '@astrojs/netlify/functions';
+import vercelStatic from '@astrojs/vercel/static';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercelStatic(),
 });
