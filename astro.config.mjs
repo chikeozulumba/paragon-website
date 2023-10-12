4b04e4305be7b7dd800b38e27204d1c3e13f3043
Chike Ozulumba <o.devcode@gmail.com>
2023-10-12 12:50:35 +0100

    update config - 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-// import netlify from '@astrojs/netlify/functions';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-//   adapter: cloudflare()
 });
