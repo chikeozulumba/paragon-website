7f532e073e06d71993a0124791662110c25ba85f
Chike Ozulumba <o.devcode@gmail.com>
2023-10-31 16:18:30 +0100

    chore: refactoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,17 @@ import vercel from "@astrojs/vercel/serverless";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    sitemap(),
-  ],
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap(), tailwind()],
   output: "server",
   adapter: vercel({
-    edgeMiddleware: true,
-  }),
+    edgeMiddleware: true
+  })
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      'primary': ['Epilogue'],
+      'body': ['"Typo Grotesk"'],
+    },
+    extend: {
+      screens: {
+        xl: "1800px",
+      },
+    },
+  },
+  plugins: [],
+};
