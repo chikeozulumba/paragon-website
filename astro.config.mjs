816d3f05b9640bb8519d0881f36807c4f80d824a
Chike Ozulumba <o.devcode@gmail.com>
2023-11-01 14:14:41 +0100

    chore: refactoring - fix build - 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,17 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sitemap(), tailwind()],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+    tailwind(),
+  ],
   output: "server",
   adapter: vercel({
-    edgeMiddleware: true
-  })
+    edgeMiddleware: true,
+  }),
 });
