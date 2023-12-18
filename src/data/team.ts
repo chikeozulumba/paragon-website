c460bdb92ab8e0f3d60732cb8980a84fce40e7ae
Chike Ozulumba <o.devcode@gmail.com>
2023-12-18 09:27:25 +0100

    chore: update founder bio

---
+++ src/data/team.ts
@@ -0,0 +1,15 @@
+
+import IJImage from "../assets/images/team/ij.jpeg"; 
+
+export const LIST_OF_TEAM = [
+  {
+    name: "Ijeoma Ofonagoro",
+    linkedIn: "https://www.linkedin.com/in/ijeomamba/",
+    image: IJImage,
+    description: `Ijeoma is a People Operations and Talent Development Leader with 13+ years of experience working with international and cross-cultural teams spanning the US, Europe, Africa, and Asia.  She has spent the majority of her career working with social enterprises, startups, and international development agencies to scale projects.`,
+  },
+  {
+    name: "Sadiq Idris",
+    description: `Sadiq has six years of experience supporting international, national, and sub-national organizations that deliver innovative and impactful solutions to clients and the people they serve. As a graduate of Mathematics from the prestigious Bayero University Kano, he veered from calculating numbers to counting social impact projects. With expertise in compliance, people and culture, and recruitment across the US and Africa, he is the go-to for responsive people polices and strategies. He is the Talent Acquisition Lead at Paragon, an avid reader, adrenaline junkie, and a seasoned hiker.`,
+  },
+];
