64ecb14d2805be47ce3e3a779594a5883adbda96
Chike Ozulumba <o.devcode@gmail.com>
2024-01-07 00:44:00 +0100

    chore: updated testimonials

---
+++ src/data/testimonials.ts
@@ -0,0 +1,14 @@
+export const TESTIMONIALS_DATA = [
+  {
+    title: undefined,
+    content: "IJ is a thoughtful strategist and coach. She was able to coach me through valuable introspection to think differently about my workplace dynamic. I was able to navigate some tricky waters and charter a path for my professional growth.",
+    author: "Manager",
+    org: "Meta",
+  },
+  {
+    title: undefined,
+    content: "Paragon has been an excellent resource for my startup. The team is responsive, creative, and thinks outside the box. They helped us determine our structure, systems, and organizational design approach.",
+    author: "Founder",
+    org: "Astur",
+  }
+]
