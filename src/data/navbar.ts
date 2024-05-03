931b0720ea3511b6446c2195b862b11e34ae545b
Chike Ozulumba <o.devcode@gmail.com>
2024-05-03 17:27:02 +0100

    fix: meeting link

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -3,7 +3,7 @@ export const HERO_NAVIGATION_ITEMS = [
   { title: "Services" },
   {
     title: "Advisory Sessions",
-    url: "https://topmate.io/ij_paragonconsulting/644248",
+    url: "https://topmate.io/paragonconsulting/644248",
     attributes: {
       target: "_blank",
     },
@@ -11,7 +11,7 @@ export const HERO_NAVIGATION_ITEMS = [
   { title: "Resources", url: "/resources" },
   {
     title: "Let's Talk",
-    url: "https://topmate.io/ij_paragonconsulting",
+    url: "https://topmate.io/paragonconsulting",
     attributes: {
       target: "_blank",
       class:
@@ -25,7 +25,7 @@ export const FOOTER_NAVIGATION_ITEMS = [
   { title: "Services" },
   {
     title: "Advisory Sessions",
-    url: "https://topmate.io/ij_paragonconsulting/644248",
+    url: "https://topmate.io/paragonconsulting/644248",
     attributes: {
       target: "_blank",
     },
@@ -33,7 +33,7 @@ export const FOOTER_NAVIGATION_ITEMS = [
   { title: "Resources", url: "/resources" },
   {
     title: "Let's Talk",
-    url: "https://topmate.io/ij_paragonconsulting",
+    url: "https://topmate.io/paragonconsulting",
     attributes: {
       target: "_blank",
       // class: "cta text-[var(--primary-color)] font-[500] px-[20px] py-[12px] rounded-[100px] bg-white lg:text-[14px] xl:text-[16px]",
@@ -50,7 +50,7 @@ export const ALT_HERO_NAVIGATION_ITEMS = [
   { title: "Services" },
   {
     title: "Advisory Sessions",
-    url: "https://topmate.io/ij_paragonconsulting/644248",
+    url: "https://topmate.io/paragonconsulting/644248",
     attributes: {
       target: "_blank",
     },
@@ -58,7 +58,7 @@ export const ALT_HERO_NAVIGATION_ITEMS = [
   { title: "Resources", url: "/resources" },
   {
     title: "Let's Talk",
-    url: "https://topmate.io/ij_paragonconsulting",
+    url: "https://topmate.io/paragonconsulting",
     attributes: {
       target: "_blank",
       class:
@@ -76,14 +76,14 @@ export const RESOURCES_HERO_NAVIGATION_ITEMS = [
   { title: "Services" },
   {
     title: "Advisory Sessions",
-    url: "https://topmate.io/ij_paragonconsulting/644248",
+    url: "https://topmate.io/paragonconsulting/644248",
     attributes: {
       target: "_blank",
     },
   }, { title: "Resources", url: "/resources" },
   {
     title: "Let's Talk",
-    url: "https://topmate.io/ij_paragonconsulting",
+    url: "https://topmate.io/paragonconsulting",
     attributes: {
       target: "_blank",
       class:
